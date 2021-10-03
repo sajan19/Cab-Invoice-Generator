112bdb59763b11f1f216e849d7e39d4749ee7c1a
Sajan <sajanmhatre29@gmail.com>
2021-10-04 02:27:23 +0530

    Test Case to calculate Minimum Fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -6,8 +6,13 @@ public class InvoiceGenerator {
     //Declare Constant Variables
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1 ;
+    private static final double MINIMUM_FARE = 5;
+
     //Method to Calculate Fare
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME ;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME ;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/InvoiceGeneratorTest.java
+++ src/test/java/InvoiceGeneratorTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceGeneratorTest {
-    //Test Case-1 Calculate correct Fare
+    //Test Case-1 Calculate Total Fare
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
         //Create an Object
@@ -14,5 +14,18 @@ public class InvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         //Assertions to check Expected and Actual Value
         Assertions.assertEquals(25, fare,0.0);
+    }
+    //Test Case to Calculate Minimum Fare
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
+        //Create an Object
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        //Declare Local Variables
+        double distance = 0.1;
+        int time = 1;
+        //Calling of Method Calculate Fare
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        //Assertions to check Expected and Actual Value
+        Assertions.assertEquals(5, fare,0.0);
     }
 }
